b27ac687a71145b5be357f9b62b6d7348c98a48a
Gregory McIntyre <gmcintyre@yourtutor.com.au>
2014-03-09 00:00:13 +1100

    Disable group creation/editing for now.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :groups, except: [:index]
+  resources :groups, except: [:index, :new, :edit, :create, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
