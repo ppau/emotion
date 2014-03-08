787a1cebc7e0b7c64ee07472f5c213f63e2ddc1a
Gregory McIntyre <gmcintyre@yourtutor.com.au>
2014-03-09 00:01:06 +1100

    Group show view.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,11 @@
 class GroupsController < ApplicationController
-  before_action :set_group, only: [:edit, :update, :destroy]
+  before_action :set_group, only: [:show, :edit, :update, :destroy]
 
   def index
     @groups = Group.all
+  end
+
+  def show
   end
 
   def new
