e045f030be1be0d8092599e19d3adef4f2934076
Gregory McIntyre <gmcintyre@yourtutor.com.au>
2014-03-09 00:57:03 +1100

    Add breadcrumbs.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ActiveRecord::Base
   has_many :group_memberships
   has_many :groups, through: :group_memberships
 
-  validates :display_name, presence: true
+  validates :display_name, presence: true, length: { minimum: 3, maximum: 20 }
 end
