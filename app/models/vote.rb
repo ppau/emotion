e2fdc3291cff5c5910abdc8bc12146d3b390ade5
Gregory McIntyre <gmcintyre@yourtutor.com.au>
2014-03-09 01:25:32 +1100

    List votes on motion page.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,4 +5,17 @@ class Vote < ActiveRecord::Base
   validate :user, presence: true
   validate :motion, presence: true
   validate :response, inclusion: { in: %w(yea nay abstain) }
+
+  def display_response
+    case response
+    when /yea/i
+      'yea'
+    when /nay/i
+      'nay'
+    when /abstain/i
+      'abstain'
+    else
+      'pending'
+    end
+  end
 end
