ebf41b829c6d1b464e3ef2ff8efb762b58542b56
Gregory McIntyre <gmcintyre@yourtutor.com.au>
2014-03-10 15:34:24 +1100

    Fix user role editing. Add flash messages.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,15 +18,15 @@ class GroupsController < ApplicationController
     if @group.save
       redirect_to groups_url, notice: 'Group was successfully created.'
     else
-      render action: 'new'
+      render action: 'new', alert: 'Group could not be created.'
     end
   end
 
   def update
     if @group.update(group_params)
-      redirect_to groups_url, notice: 'Group was successfully updated.'
+      redirect_to edit_group_url(@group), notice: 'Group was successfully updated.'
     else
-      render action: 'edit'
+      render action: 'edit', alert: 'Group could not be updated.'
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,15 +14,15 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to users_url, notice: 'User was successfully created.'
     else
-      render action: 'new'
+      render action: 'new', alert: 'User could not be created.'
     end
   end
 
   def update
     if @user.update(user_params)
-      redirect_to users_url, notice: 'User was successfully updated.'
+      redirect_to edit_user_url(@user), notice: 'User was successfully updated.'
     else
-      render action: 'edit'
+      render action: 'edit', alert: 'User could not be updated.'
     end
   end
 
@@ -35,6 +35,9 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password)
+    if params[:user] and params[:user][:password].blank?
+      params[:user].delete(:password)
+    end
+    params.require(:user).permit(:name, :role, :email, :password, group_ids: [])
   end
 end
